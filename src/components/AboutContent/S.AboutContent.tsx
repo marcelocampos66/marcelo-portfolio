0c9a97d51ff663c4240b11d6ca44a121b78b9edc
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-10-20 13:41:49 -0300

    Adicionei animacoes nas paginas de Home e About

--- src/components/AboutContent/S.AboutContent.tsx
+++ src/components/AboutContent/S.AboutContent.tsx
@@ -1,6 +1,16 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 class Styled {
+
+  fadeInAnimation = keyframes`
+    0% { opacity: 0.1; }
+    100% { opacity:1; }
+  `;
+
+  zoomInAnimation = keyframes`
+    0% { transform: scale(0.2, 0.2); }
+    100% { transform: scale(1, 1); }
+  `;
 
   Main = styled.main`
     display: flex;
@@ -25,6 +35,7 @@ class Styled {
     width: 300px;
     height: 300px;
     border-radius: 100%;
+    animation: ${this.fadeInAnimation} 3s;
   `;
 
   DivContent = styled.div`
@@ -33,18 +44,20 @@ class Styled {
     align-items: flex-start;
     flex-direction: column;
     width: 40%;
+    animation: ${this.zoomInAnimation} 1s;
   `;
 
   H1 = styled.h1`
     color: ${(props) => props.theme.colors.text};
-    margin-bottom: 3%;
+    margin-bottom: 5%;
   `;
 
   P = styled.p`
     width: 80%;
     font-size: 18px;
     color: ${(props) => props.theme.colors.text};
-    margin-bottom: 1%;
+    margin-bottom: 5%;
+    text-align: justify;
   `;
 
 }
--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Styled from './S.HomeContent';
+import me from '../../images/me-home.jpg';
 
 const HomeContent: React.FC = () => {
   return (
     <Styled.Main>
-      HOME
+      <Styled.DivH1>
+        <Styled.H1>
+          Hello! I'm Marcelo.
+        </Styled.H1>
+      </Styled.DivH1>
+      <Styled.Img
+        src={ me }
+        alt="minha foto"
+      />
     </Styled.Main>
   );
 }
